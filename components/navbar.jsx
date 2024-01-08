b12caa1528b6e4159f73ce51af4c7234e9aee9d1
Ferrand Chester Noche <fdnoche@up.edu.ph>
2024-01-08 21:50:00 +0800

    NavBar button Fix

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import '../styles/navbar.css'
+import '../styles/navbar.css';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <ul className="nav-list">
-        <li><a href="home">Home</a></li>
-        <li><a href="booking">Booking</a></li>
-        <li><a href="employeemgmt">Employee Management</a></li>
-        <li><a href="guestmgmt">Guest Management</a></li>
-        <li><a href="servicesmgmt">Services Management</a></li>
-        <li><a href="login">Login</a></li>
+        <li><a href="/home">Home</a></li>
+        <li><a href="/booking">Booking</a></li>
+        <li><a href="/employeemgmt">Employee Management</a></li>
+        <li><a href="/guestmgmt">Guest Management</a></li>
+        <li><a href="/servicesmgmt">Services Management</a></li>
+        <li><a href="/login">Login</a></li>
       </ul>
     </nav>
   );
-}
+};
 
 export default Navbar;
